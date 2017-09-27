23cac265a56c3cebd7d3bac47dadfb60eaa56b32
djtrin1ty <abrahamjum@gmail.com>
2017-09-27 13:58:02 -0700

    fix bugs and mobile optimize

--- js/menu.js
+++ js/menu.js
@@ -2,22 +2,22 @@ $(document).ready(function() {
     $('.breakfast-btn').click(function() {
         $('.breakfast').siblings().removeClass('reveal');
         $('.breakfast').addClass('reveal');
-        $('.btn').addClass('reveal');
+        $('#modalBtn').addClass('reveal');
     })
     $('.burgers-btn').click(function() {
         $('.burgers').siblings().removeClass('reveal');
         $('.burgers').addClass('reveal');
-        $('.btn').addClass('reveal');
+        $('#modalBtn').addClass('reveal');
     })
     $('.platters-btn').click(function() {
         $('.platters').siblings().removeClass('reveal');
         $('.platters').addClass('reveal');
-        $('.btn').addClass('reveal');
+        $('#modalBtn').addClass('reveal');
     })
     $('.sideorders-btn').click(function() {
         $('.side-orders').siblings().removeClass('reveal');
         $('.side-orders').addClass('reveal');
-        $('.btn').addClass('reveal');
+        $('#modalBtn').addClass('reveal');
     })
 });
 
